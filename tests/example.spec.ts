72b50befd50caa4fca7e2607203108eded29c8ba
ArtemNovosel <arteimn@mail.ru>
2023-05-17 19:08:47 +0500

    TestAutorization

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,10 +1,43 @@
 import { test, expect } from '@playwright/test';
 
 
-test('has title', async ({ page }) => {
+test('authorization', async ({ page }) => {
+  //переходим на сайт
   await page.goto('https://sima-land.ru/');
 
-  await expect(page).toHaveTitle(/Сима-ленд — интернет-магазин товаров для комфортной жизни/);
-  await page.screenshot({path: 'scrin.png'});
+  //В хедере нажать на кнопку "Войти"
+  await page.getByText('Войти').click();
+
+  //Ввести логин: qa_test@test.ru
+  await page
+    .locator('//*[@class="kfX46B bTTMKM"]').nth(0)
+    .fill("qa_test@test.ru");
+    
+    //Ввести пароль: qa_test
+  await page
+    .locator('//*[@type="password"]')
+    .fill("qa_test");
+
+    //Нажать кнопку "Войти"
+  await page.locator('//*[@class="Xqv8JY huJTmi FGtHop NkchRg RgDBpc"]').click();
+
+    //Проверить, что авторизация прошла успешно
+  await page.getByText('Профиль').click(); //нажимаем кнопку профиль
+  await expect(page).toHaveURL(/.*cabinet/); //проверяем что мы в личной кабинете
+  await page.close();
+});
+
+
+// тест записанный через codegen
+test('test', async ({ page }) => {
+  await page.goto('https://sima-land.ru/');
+  await page.getByText('Войти').click();
+  await page.getByTestId('login-field').getByTestId('text-field:field').click();
+  await page.getByTestId('login-field').getByTestId('text-field:field').fill('qa_test@test.ru');
+  await page.getByTestId('password-field').getByTestId('text-field:field').click();
+  await page.getByTestId('password-field').getByTestId('text-field:field').fill('qa_test');
+  await page.getByTestId('button').click();
+  await page.getByTestId('nav-item:cabinet').getByTestId('link').click();
+  await expect(page).toHaveURL(/.cabinet/);
   await page.close();
 });
